f70512779de0c5eeaf81a2f20e07faf99f5b984c
Ebubekir Nazlı <101016149+Ebu13@users.noreply.github.com>
2023-08-27 17:41:59 +0300

    new change

---
+++ ReadME.md
@@ -0,0 +1,1 @@
+You need to go to the address "http://127.0.0.1:8000/rows" for the API created for the data in the Rows table
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
 
 app = FastAPI()
 
+# CORS ayarlarını yapılandırma
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Tüm kaynaklardan erişime izin vermek için "*" kullanabilirsiniz. Daha güvenli bir yapı için doğru etki alanlarını belirtin.
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/rows")
+def get_rows():
+    conn = sqlite3.connect('database.sqlite3')
+    c = conn.cursor()
+    c.execute("SELECT id, row_data FROM rows")
+    rows = c.fetchall()
+    conn.close()
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    formatted_rows = [{"id": row[0], "row_data": row[1]} for row in rows]
+    return {"rows": formatted_rows}

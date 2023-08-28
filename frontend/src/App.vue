af6fb231b15a533338021757cd9c3723e4f7c45e
Ebubekir Nazlı <101016149+Ebu13@users.noreply.github.com>
2023-08-28 14:07:45 +0300

    new change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,113 @@
 <template>
   <div>
-    <h1>Typing Game</h1>
-    <TextArea :rows="rows" :count="count"></TextArea>
-    <InputText v-model="metin" />
-    <p>{{ metin }}</p>
-    <button @click="increaseCount">{{ count }}</button>
+    <div v-if="!gameStarted">
+      <h1>Typing Game</h1>
+      <button @click="startGame">Başla</button>
+    </div>
+    <div v-else>
+      <p>{{ currentSentence }}</p>
+      <input v-model="inputText" @keyup.enter="checkInput" :disabled="gameEnded" placeholder="Metin girin"
+        style="margin: 10px; font-size: 20px;">
+      <p v-if="isCorrect">Doğru!</p>
+      <p v-else-if="isIncorrect">Hatalı!</p>
+      <div style="display: flex; flex-direction: column; align-items: center;">
+        <button v-if="!gameEnded" @click="nextSentence">Sonraki Cümle</button>
+        <button v-else @click="restartGame">Tekrar Oyna</button>
+        <button v-if="gameEnded" @click="exitGame" >Çıkış</button>
+      </div>
+      <div>
+        <p v-if="gameEnded">Skor: {{ getFormattedTime(currentTime) }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import TextArea from './components/TextArea.vue';
-import InputText from './components/InputText.vue';
-
 export default {
-  components: {
-    TextArea,
-    InputText,
-  },
   data() {
     return {
-      rows: [],
-      count: 0,
-      metin:"",
+      sentences: [],
+      currentSentenceIndex: 0,
+      currentSentence: '',
+      inputText: '',
+      isCorrect: false,
+      isIncorrect: false,
+      currentTime: 0,
+      gameStarted: false,
+      gameEnded: false,
     };
   },
   mounted() {
     fetch('http://127.0.0.1:8000/rows')
       .then(response => response.json())
       .then(data => {
-        this.rows = data.rows;
+        this.sentences = data.rows;
+      })
+      .catch(error => {
+        console.log(error);
       });
+
+    setInterval(() => {
+      if (this.gameStarted && !this.gameEnded) {
+        this.currentTime++;
+      }
+    }, 1000);
   },
   methods: {
-    increaseCount() {
-      this.count++;
+    startGame() {
+      this.gameStarted = true;
+      this.currentSentenceIndex = 0;
+      this.currentSentence = this.sentences[this.currentSentenceIndex].row_data;
+      this.inputText = '';
+      this.isCorrect = false;
+      this.isIncorrect = false;
+      this.currentTime = 0;
+      this.gameEnded = false;
+    },
+    exitGame() {
+      this.gameStarted = false;
+      this.gameEnded = false;
+      this.inputText = '';
+      this.currentSentenceIndex = 0;
+      this.currentSentence = '';
+      this.isCorrect = false;
+      this.isIncorrect = false;
+      this.currentTime = 0;
+    },
+    checkInput() {
+      if (this.inputText === this.currentSentence) {
+        this.isCorrect = true;
+        this.isIncorrect = false;
+        this.nextSentence();
+      } else {
+        this.isCorrect = false;
+        this.isIncorrect = true;
+      }
+    },
+    nextSentence() {
+      this.currentSentenceIndex++;
+      if (this.currentSentenceIndex < this.sentences.length) {
+        this.currentSentence = this.sentences[this.currentSentenceIndex].row_data;
+        this.inputText = '';
+        this.isCorrect = false;
+        this.isIncorrect = false;
+        if (!isNaN(this.sentences[this.currentSentenceIndex].time_to_next)) {
+          this.currentTime += this.sentences[this.currentSentenceIndex].time_to_next;
+        }
+      } else {
+        this.gameEnded = true;
+      }
+    },
+    restartGame() {
+      this.startGame();
+    },
+    getFormattedTime(seconds) {
+      if (isNaN(seconds)) {
+        return "00:00";
+      }
+      const minutes = Math.floor(seconds / 60);
+      const remainingSeconds = seconds % 60;
+      return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
     },
   },
 };
